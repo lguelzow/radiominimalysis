ef1791e6ad8f60778abcf20810ee1d8720196b21
lguelzow <lukas.guelzow@kit.edu>
2025-12-17 18:50:35 +0100

    add missing file

---
+++ radiominimalysis/scripts/CR_cherenkov_comparison.py
@@ -0,0 +1,39 @@
+import argparse
+
+import numpy as np
+
+from RadioAnalysis.input_output import coreas_reader
+from RadioAnalysis.modules.CoREASanalysis import gauss_sigmoid_param as GS_param
+from RadioAnalysis.modules.reconstruction import geometry
+from RadioAnalysis.modules.reconstruction import signal_emissions
+from RadioAnalysis.modules.reconstruction import RAY_parallel_functions
+from RadioAnalysis.modules.CoREASanalysis import ldf_evaluation as LDF_eval
+
+from matplotlib import pyplot as plt
+
+from RadioAnalysis.framework import factory
+from RadioAnalysis.framework.parameters import showerParameters as shp
+
+from RadioAnalysis.utilities import parsers
+
+# get command line arguments
+# read out in separate function
+args = parsers.fit_arguments()
+
+ #print(args)
+
+# initialise factory object for saving results
+readout_factory = factory.EventFactory()
+
+readout_factory.read_events_from_file(args.paths[0])
+
+events = readout_factory.get_events()
+
+alpha = factory.get_parameter(events, shp.geomagnetic_angle)
+mask = alpha > 0.5
+
+print("Applying geomagnetic angle filter...")
+print(f"Amount of events with sufficiently large geomagnetic angle: {len(events[mask])}/{len(events)}")
+events = events[mask]
+
+GS_param.evaluate_fitted_cherenkov_radius(events, args)
